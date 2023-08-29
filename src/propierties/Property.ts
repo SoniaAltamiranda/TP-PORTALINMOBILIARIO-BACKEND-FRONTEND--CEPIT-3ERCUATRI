693afd46d31fb0d5adc999ac4153d7291df67c61
SoniaAltamiranda <sonia.altamiranda10@gmail.com>
2023-08-28 22:38:07 -0300

    iniciando el proyecto base

---
+++ src/propierties/Property.ts
@@ -0,0 +1,10 @@
+export interface Property {
+  id: number;
+  title: string;
+  location: string;
+  type: string;
+  rooms: number;
+  description: string;
+  price: number;
+  images: string[];
+}
